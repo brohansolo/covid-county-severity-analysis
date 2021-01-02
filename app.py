5d9615231d8f06b5983b9e0a1a2f3cd7d8057f95
rohan <rohanbansal406@gmail.com>
2021-01-01 21:39:05 -0600

    Preliminary Files Push

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+PAGES = {
+        "Preliminary Findings": findings,
+        "Severity Analysis": analysis
+        }
+
+st.sidebar.title('Covid-19 County Impact')
+selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+page = PAGES[selection]
+page.app()
---
+++ setup.sh
@@ -0,0 +1,15 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"rohanbansal406@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
+
